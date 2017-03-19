5a51e8ad8611b12533af0cb82737cd4bf1f9bfe6
Thomas Puppe <info@thomaspuppe.de>
2017-03-20 00:36:33 +0100

    implement basic timer

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const POMODORO_STATES = {
+	WORK: 'work',
+	REST: 'rest'
+};
+const WORKING_TIME_LENGTH_IN_MINUTES = 25;
+const RESTING_TIME_LENGTH_IN_MINUTES = 5;
+
+new Vue({
+	el: '#app',
+	data: {
+		minute: WORKING_TIME_LENGTH_IN_MINUTES,
+		second: 0,
+		pomodoroState: POMODORO_STATES.WORK,
+		timestamp: 0
+	},
+	methods: {
+		start: function () {
+			this._tick();
+			this.interval = setInterval(this._tick, 1000);
+		},
+		_tick: function() {
+			if (this.second !== 0) {
+				this.second--;
+				return;
+			}
+			if (this.minute !== 0) {
+				this.minute--;
+				this.second = 59;
+				return;
+			}
+		}
+	}
+
+});
